397b94e675520886b1dad08435e10b11260ed729
Berke Ceylan <berkeceylan@sabanciuniv.edu>
2025-09-25 17:41:00 +0300

    Sound player UI template

--- src/components/SoundPlayer/SoundPlayer.jsx
+++ src/components/SoundPlayer/SoundPlayer.jsx
@@ -1,9 +1,24 @@
-export default SoundPlayer
+import "./SoundPlayer.css";
 
-function SoundPlayer() {
+export default function SoundPlayer() {
   return (
     <div className="sound-player">
-      <p>Sound Player Component</p>
+      <h2 className="sound-header">Background Sound</h2>
+      <div className="sound-controls">
+        <p className="sound-label">Session Sound</p>
+        <select>
+          <option value="none">No sound</option>
+          <option value="rain">Rain</option>
+          <option value="fire">Crackling</option>
+        </select>
+      </div>
+      <div className="sound-controls">
+        <p className="sound-label">Break Sound</p>
+        <select>
+          <option value="none">No sound</option>
+          <option value="piano">Piano</option>
+        </select>
+      </div>
     </div>
-  )
+  );
 }
