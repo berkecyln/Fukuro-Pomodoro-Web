c3950fc87abefb3550544a6b23550d1e25864477
Berke Ceylan <berkeceylan@sabanciuniv.edu>
2025-09-25 17:41:41 +0300

    progress bar addition

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Timer from "./components/Timer/Timer.jsx";
 import TodoList from "./components/Todo/TodoList.jsx";
 import SoundPlayer from "./components/SoundPlayer/SoundPlayer.jsx";
+import ProgressBar from "./components/ProgressBar/ProgressBar.jsx";
 
 function App() {
   return (
@@ -10,9 +11,16 @@ function App() {
       <div className="app">
         <h1 className="app-title">Fukuro App</h1>
         <div className="components-container">
-          <SoundPlayer />
-          <Timer />
-          <TodoList />
+          <div className="left-section">
+            <SoundPlayer />
+            <ProgressBar />
+          </div>
+          <div className="middle-section">
+            <Timer />
+          </div>
+          <div className="right-section">
+            <TodoList />
+          </div>
         </div>
       </div>
     </>
